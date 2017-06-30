8b83469599c41ce21359820bd6d354d2073acc8a
kemo <kemal.delalic@gmail.com>
2017-06-30 09:44:33 +0200

    Include compare URLs

--- extension.js
+++ extension.js
@@ -1,12 +1,19 @@
 chrome.webRequest.onBeforeRequest.addListener(
-  function(request) {
-	var url = new URL(request.url);
-	
-	if ( ! url.searchParams.has('w')){
-		url.searchParams.set('w', 1);
+	function(request) {
+		var url = new URL(request.url);
 
-		return {redirectUrl: url.href};
-	}
-  },
-  { urls: [ '*://github.com/*/commits/*', '*://github.com/*/pull/*/files'] },
-  ['blocking']);
+		if ( ! url.searchParams.has('w')){
+			url.searchParams.set('w', 1);
+
+			return {redirectUrl: url.href};
+		}
+	},
+	{
+		urls: [
+			'*://github.com/*/commits/*',
+			'*://github.com/*/pull/*/files',
+			'*://github.com/*/compare/*'
+		]
+	},
+	['blocking']
+);
